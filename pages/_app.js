277a296075330f7d04c46d4952bc7337cd08e3e0
Munshya Zoya M. Masocha <zmasocha@comesa.int>
2025-09-07 01:02:57 +0200

    Analytics for vercel.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 // pages/_app.js
 import 'katex/dist/katex.min.css'; // KaTeX CSS for math rendering
+import { Analytics } from '@vercel/analytics/next';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
