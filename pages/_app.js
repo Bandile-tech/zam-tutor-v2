7515861d5e96c906b988e145ae4405e6a619b86f
bandilemasocha <48050942-bandilemasocha@users.noreply.replit.com>
2025-09-28 01:40:32 +0000

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
-import 'katex/dist/katex.min.css'; // KaTeX CSS for math rendering
+import '../styles/globals.css';       // Tailwind + custom global styles
+import 'katex/dist/katex.min.css';    // KaTeX CSS for math rendering
 import { Analytics } from '@vercel/analytics/next';
 
 export default function MyApp({ Component, pageProps }) {
